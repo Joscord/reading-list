e471aaf551e9f91fdf0a4728fa882e9088584136
joscord <josecorderodev@gmail.com>
2022-02-24 19:08:30 -0300

    feat: fetched books from firestore in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+// Importamos db
+import { db } from '../firebase/config';
+// Debemos importar las funciones específicas que queremos usar de Firebase
+import { collection, getDocs } from 'firebase/firestore';
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
 
 export default function Home() {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState(null);
+  // Hacemos el fetch de la data usando useEffects
+  useEffect(() => {
+    // Creamos una referencia para una colección. El método colecction recibe dos argumentos: el primero es la base de datos a la que nos queremos conectar (db). El segundo es el nombre de la colección (books).
+    const ref = collection(db, 'books');
+    // Este método es para obtener los documentos pero retorna una promesa
+    const getDocuments = async reference => {
+      try {
+        // Obtenemos el snapshot
+        const snapshot = await getDocs(reference);
+        let results = [];
+        snapshot.docs.forEach(doc => results.push({id: doc.id, ...doc.data()}));
+        // Cambiamos el estado
+        setBooks(results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getDocuments(ref);
+  
+    return () => {
+      
+    }
+  }, [])
   
   return (
     <div className="App">
