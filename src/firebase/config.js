280b5d6bd53f09544350788e4f3d6a8a08f5559d
joscord <josecorderodev@gmail.com>
2022-02-24 18:07:38 -0300

    feat: created config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+// Objeto de configuración facilitado por Firebase
+const firebaseConfig = {
+	apiKey: 'AIzaSyD4qP3rvv0QyV4xPSU37h5W4NMByScgSqs',
+	authDomain: 'reading-list-4ffc5.firebaseapp.com',
+	projectId: 'reading-list-4ffc5',
+	storageBucket: 'reading-list-4ffc5.appspot.com',
+	messagingSenderId: '254490772120',
+	appId: '1:254490772120:web:0a9b311f4d23f767ebc6ed',
+};
