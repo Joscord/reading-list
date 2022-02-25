a678e2c874e15faa9ec0a4e1df244796944bd49e
joscord <josecorderodev@gmail.com>
2022-02-25 15:33:52 -0300

    feat: modified BookList.js so it can delete a document from a Firestore collection

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,14 @@
+import { db } from '../firebase/config';
+// El método doc nos da una referencia a un documento (similar a collection que nos da una referencia a una colección)y el método deleteDoc nos permite borrar un documento usando una referencia a éste
+import { doc, deleteDoc} from 'firebase/firestore'
+
 export default function BookList({ books }) {
 
   const handleClick = async (id) => {
-    console.log(id)
+    // Primero obtenemos una referencia a un documento. El primer argumento es la base de datos, la segunda es la colección y el tercer argumento el id del documento que deseamos referenciar
+    const ref = doc(db, 'books', id);
+    // Esperamos borrar el documento
+    await deleteDoc(ref);
   }
 
   return (
