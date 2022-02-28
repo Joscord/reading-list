2d815bfee142ef534fb481eb2ab17e980d9b253e
joscord <josecorderodev@gmail.com>
2022-02-28 17:22:03 -0300

    feat: modified useLogin.js to use the auth context

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { auth } from "../firebase/config";
-// Importamos la función para el loggin
 import { signInWithEmailAndPassword } from "firebase/auth";
+// Importamos el hook para utilizar el contexto
+import { useAuthContext } from './useAuthContext';
+
 
 export const useLogin = () => {
     const [error, setError] = useState(null);
+    // Destructuramos la función de despacho
+    const { dispatch } = useAuthContext();
 
     const login = async (email, password) => {
-        // Reseteamos el error, haciéndolo nulo. 
         setError(null);
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            console.log('user signed up:',userCredential.user);
+            // Luego de loguear en Firebase, disparamos una acción para modificar el estado de nuestra app
+            dispatch({type: 'LOGIN', payload: userCredential.user});
         } catch (err) {
             setError(err.message);
         }
