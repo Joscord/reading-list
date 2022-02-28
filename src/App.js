fcb1e06222420d7f7c5f8231d62c5a0368b60406
joscord <josecorderodev@gmail.com>
2022-02-28 17:41:41 -0300

    feat: modified App.js to guard routes based on auth state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+// Importamos el hook para usar el contexto
+import { useAuthContext } from './hooks/useAuthContext';
+// Importamos el componente de redirección
 
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+	// Obtenemos el usuario y authIsReady del contexto
+	const { user, authIsReady } = useAuthContext();
+
+	return (
+		<div className='App'>
+			{authIsReady && (
+				<BrowserRouter>
+					<Navbar />
+					<Switch>
+						<Route exact path='/'>
+							{user ? <Home /> : <Redirect to={'/login'}/>}
+						</Route>
+						<Route path='/signup'>
+							{!user ? <Signup /> : <Redirect to={'/'}/>}
+						</Route>
+						<Route path='/login'>
+							{!user ? <Login /> : <Redirect to={'/'}/>}
+						</Route>
+					</Switch>
+				</BrowserRouter>
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
